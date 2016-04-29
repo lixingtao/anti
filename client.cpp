803d2771bea21d42b7dec3dcdf7525739ccb3ed2
lixingtao <lixingtao.li@gmail.com>
2016-04-29 17:04:15 +0800

    next

--- client.cpp
+++ client.cpp
@@ -67,7 +67,7 @@ int main(int argc, char **argv)
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	//servaddr.sin_port = htons(SERV_PORT);
-	servaddr.sin_port = htons(14);
+	servaddr.sin_port = htons(9877);
 	inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
 
 	connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
